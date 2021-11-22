2aa5f5b25fa14e9025ac4be472556ff10cd58b84
Don <donavan.olsen@gmail.com>
2021-11-21 18:27:36 -0800

    Refacored code into codebehind file

---
+++ Client/Shared/MainLayout.razor.cs
@@ -0,0 +1,22 @@
+﻿using BlazorApp.Client.Utils;
+using BlazorApp.Client.Utils.Constants;
+using Blazored.Modal;
+using Blazored.Modal.Services;
+using Microsoft.AspNetCore.Components;
+
+namespace BlazorApp.Client.Shared
+{
+    public partial class MainLayout : LayoutComponentBase
+    {
+        [CascadingParameter] public IModalService? Modal { get; set; }
+
+        private ModalOptions ModalOpts
+        {
+            get
+            {
+                return ModalOptionsFactory
+                    .GetOptions(ModalTypes.Default);
+            }
+        }
+    }
+}
