1c1cc3c92aa94035939c973550eed1ce7dbb94d0
Don <donavan.olsen@gmail.com>
2021-11-29 12:40:21 -0800

    fixed top menu layout issue on small screens
    created TopMenu component to reuse between NavMenu and MainLayout
    dark-mode toggle is now visible when navbar is collapsed
    Top menu flows smothly through various brake points
    
    fixes #45

---
+++ Client/Utils/Constants/ThemeOptions.cs
@@ -0,0 +1,20 @@
+﻿namespace BlazorApp.Client.Utils.Constants
+{
+
+    public static class ThemeOptions
+    {
+        private static readonly List<string> themes 
+            = new List<string>()
+            { "light-mode", "dark-mode" };
+
+        public static List<string> Themes 
+        { 
+            get 
+            {
+                return themes;
+            }
+        }
+            
+
+    }
+}
