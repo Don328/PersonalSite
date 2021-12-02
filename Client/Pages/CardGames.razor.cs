9cde762bafd65b7c4dffadd34d3585b76b480fcd
Don <donavan.olsen@gmail.com>
2021-12-02 10:39:34 -0800

    crated codebehind file

---
+++ Client/Pages/CardGames.razor.cs
@@ -0,0 +1,17 @@
+﻿using AlgoCards.Enums;
+using AlgoCards.Models;
+using Microsoft.AspNetCore.Components;
+
+namespace BlazorApp.Client.Pages
+{
+    public partial class CardGames : ComponentBase
+    {
+        private Deck deck = new();
+
+        protected override async Task OnInitializedAsync()
+        {
+            deck = new Deck(DeckType.Shuffled);
+            await Task.CompletedTask;
+        }
+    }
+}
